6a3f9787a27c83be4f1b30471100d40267ef1195
PavelDymkov <dymkov86@gmail.com>
2018-11-25 18:21:11 +0300

    dev

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var spaces = /\s+/g;
 module.exports = function createFor(plugin, defaultOptions) {
     return function isEqual(input, expected, options) {
         var babelOptions = {
-            plugins: [plugin, options || defaultOptions || {}]
+            plugins: [[plugin, options || defaultOptions || {}]],
         };
         var output = babel.transform(input, babelOptions).code;
 
